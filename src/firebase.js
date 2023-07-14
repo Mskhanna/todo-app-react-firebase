f65b09792166cb24d925ebbd6e6c1581a7cfb1f6
Manu Khanna <2manukhanna@gmail.com>
2023-07-14 15:24:06 -0400

    Firebase Update

--- src/firebase.js
+++ src/firebase.js
@@ -7,14 +7,14 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBWVLuiV-96ctHydwvOz-KCjfxanWojK3o",
-  authDomain: "to-do-app-f94ec.firebaseapp.com",
-  projectId: "to-do-app-f94ec",
-  storageBucket: "to-do-app-f94ec.appspot.com",
-  messagingSenderId: "367925785429",
-  appId: "1:367925785429:web:0d56d9367521897f3c75dc",
-  measurementId: "G-QY4GL6D6JC"
+  apiKey: "AIzaSyDBIRfo5yAtIAJAm0E63E65ByCGrcncNmA",
+  authDomain: "todo-app-self.firebaseapp.com",
+  projectId: "todo-app-self",
+  storageBucket: "todo-app-self.appspot.com",
+  messagingSenderId: "745667176843",
+  appId: "1:745667176843:web:73e8ee8770327b58d3678e"
 };
 
 // Initialize Firebase
